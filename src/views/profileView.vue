36e03f602c145dda6e3b4c4b03b4d087dafae6a3
moha <mohamed.koujil.7e7@itb.cat>
2024-05-22 10:23:47 +0200

    login and register pages

---
+++ src/views/profileView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="user">
+      <h2>{{ user.name }}</h2>
+      <img :src="user.profilePicture" alt="Profile Picture">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      user: Object
+    }
+  };
+  </script>
+  
