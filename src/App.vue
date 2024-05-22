f1a2a3142511733e2f79389c7635775826ee5af2
moha <mohamed.koujil.7e7@itb.cat>
2024-05-22 09:19:17 +0200

    app.vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <nav>
-  <router-link to="/">Home</router-link> |  
-  <router-link to="/profile">Profile</router-link> |
-  <router-link to="/edit-profile">Edit Profile</router-link>
-</nav>
+  </nav>
   <router-view/>
 </template>
 
